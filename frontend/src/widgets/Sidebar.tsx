06b0b5a3747bd467b0ba677d606cc8caded99107
nolesh11 <leshkanogay@gmail.com>
2026-03-27 00:56:50 +0500

    Replace navlink with component

--- frontend/src/widgets/Sidebar.tsx
+++ frontend/src/widgets/Sidebar.tsx
@@ -1,50 +1,26 @@
-import { NavLink } from "react-router-dom";
-import {
-  ChartColumn,
-  // ChevronRight,
-  Kanban,
-  LayoutDashboard,
-  ListTodo,
-  PocketKnife,
-  Settings,
-} from "lucide-react";
+import { PocketKnife } from "lucide-react";
 import { SidebarNavLink } from "./features/SidebarNavLink";
+import { navItems } from "./config/nav";
 
 export function Sidebar() {
   return (
-    <div className="[grid-area:sidebar] p-8">
+    <div className="[grid-area:sidebar] p-8 bg-[#101428]">
       <div className="flex gap-4">
         <div className="bg-[#396FC8] p-2 rounded-md">
-          <PocketKnife color="#fff"/>
+          <PocketKnife color="#fff" />
         </div>
         <h2 className="self-center">Task manager app</h2>
       </div>
-      <div className="flex flex-col">
-        <SidebarNavLink path='/' title="Dashboard" icon={LayoutDashboard}/>
-        {/* <div className="flex">
-          
-          <NavLink to="/">Dashboard</NavLink>
-          <div>
-            <ChevronRight />
-          </div>
-        </div> */}
-        <div className="flex">
-          <ListTodo />
-          <NavLink to="/projects">Projects</NavLink>
-        </div>
-        <div className="flex">
-          <Kanban />
-          <NavLink to="/board">Board</NavLink>
-        </div>
-        <div className="flex">
-          <ChartColumn />
-          <NavLink to="/analytics">Analytics</NavLink>
-        </div>
-        <div className="flex">
-          <Settings />
-          <NavLink to="/settings">Settings</NavLink>
-        </div>
-      </div>
+      <nav className="flex flex-col">
+        {navItems.map((item) => (
+          <SidebarNavLink
+            key={item.path}
+            path={item.path}
+            title={item.title}
+            icon={item.icon}
+          />
+        ))}
+      </nav>
     </div>
   );
 }
