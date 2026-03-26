e5ce666adedbd3fedfa0490b68f598d4a9a8593d
nolesh11 <leshkanogay@gmail.com>
2026-03-27 00:57:26 +0500

    Fixed style

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from "../widgets/Sidebar";
 
 export function AppLayout() {
   return (
-    <div className="grid grid-cols-[.2fr_1fr] grid-rows-[1fr_1fr] [grid-template-areas:'sidebar_header'_'sidebar_main']">
+    <div className="grid grid-cols-[.2fr_1fr] grid-rows-[1fr_1fr] [grid-template-areas:'sidebar_header'_'sidebar_main'] bg-[#0D0E14] text-[#EEE3E6] h-screen">
       <Header />
       <Sidebar />
       <main className="[grid-area:main]">
