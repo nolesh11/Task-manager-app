c4b5716d6a265345cc8704a71006a9a6cca481c3
nolesh11 <leshkanogay@gmail.com>
2026-03-27 00:56:25 +0500

    Cretaed navlink component

--- frontend/src/widgets/features/SidebarNavLink.tsx
+++ frontend/src/widgets/features/SidebarNavLink.tsx
@@ -1,4 +1,4 @@
-import { type LucideIcon } from "lucide-react";
+import { ChevronRight, type LucideIcon } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
 type NavProps = {
@@ -9,9 +9,21 @@ type NavProps = {
 
 export function SidebarNavLink({ path, title, icon: Icon }: NavProps) {
   return (
-      <NavLink to={path} className={({ isActive }) => `flex gap-1 ${isActive ? "bg-[#3B82F6]" : "bg-transparent"}` }>
-        <Icon />
-        <span>{title}</span>
-      </NavLink>
+    <NavLink
+      to={path}
+      className={({ isActive }) =>
+        `flex justify-between gap-1 ${isActive ? "bg-[#3B82F6] " : "bg-transparent"}`
+      }
+    >
+      {({ isActive }) => (
+        <>
+          <div className="flex">
+            <Icon />
+            <span>{title}</span>
+          </div>
+          <div>{isActive && <ChevronRight color='#fff'/>}</div>
+        </>
+      )}
+    </NavLink>
   );
 }
