1b62149b14c9c5d64d9acfa68564f2f5facc7384
nolesh11 <leshkanogay@gmail.com>
2026-03-27 00:57:07 +0500

    Added title of page

--- frontend/src/widgets/Header.tsx
+++ frontend/src/widgets/Header.tsx
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom"
+import { navItems } from "./config/nav"
+
 export function Header() {
+  const location = useLocation();
+  const currentItem = navItems.find((title) => title.path === location.pathname)
   return (
     <div className="[grid-area:header]">
-      <header>Header</header>
+      <h2>{currentItem?.title}</h2>
     </div>
   )
 }
